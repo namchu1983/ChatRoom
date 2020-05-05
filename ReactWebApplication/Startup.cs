b53d97924c8cccf1f656095242c348f5ac39aa4e
nam <namchu1983@gmail.com>
2020-05-05 17:02:31 +0200

    remove google sign in for tests

--- ReactWebApplication/Startup.cs
+++ ReactWebApplication/Startup.cs
@@ -40,11 +40,12 @@ namespace ReactWebApplication
 
             services.AddAuthentication()
                 .AddIdentityServerJwt()
-                .AddGoogle(options =>
-                {
-                    options.ClientId = "";
-                    options.ClientSecret = "";
-                });
+                //.AddGoogle(options =>
+                //{
+                //    options.ClientId = "";
+                //    options.ClientSecret = "";
+                //})
+                ;
 
             services.AddControllersWithViews();
             services.AddRazorPages();
